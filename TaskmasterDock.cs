804347c6d43a0fd57d47004bc0d14c39c57ca9b9
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-10 10:12:54 +0300

    Enhanced the behaviour with: -Option for LowPerformance mode where we don't start new tweens every frame (looks a bit janky but uses less resources ~2%-points in average setup) -Finalized the SortDirection.Left behaviour, works perfectly -Added in support for RegularSizeChildren option that also enhances performance by a small margin (we can find more improvements there if we put our mind to it, now it is barely more performant)

--- TaskmasterDock.cs
+++ TaskmasterDock.cs
@@ -13,12 +13,31 @@ public class TaskmasterDock : Control
     [Get("VBox/OrganizableContainer")] private OrganizableContainer _container;
     [Get("VBox/Top/IsSortedValue")] private Label _isSortedValue;
     [Get("VBox/Top/HoverIdxValue")] private Label _hoverIdxValue;
+    [Get("VBox/Top/Regular")] private Button _regularButton;
+    [Get("VBox/Top/Dir")] private Button _dirButton;
     
     public override void _Ready()
     {
         this.SetupWayfarer();
-        
-        
+
+        try
+        {
+            _regularButton.Connect("button_up", this, nameof(OnRegularButtonPressed));
+            _regularButton.Text = "Regular: " + _container.RegularSizeChildren;
+        }
+        catch (Exception e)
+        {
+            
+        }
+        try
+        {
+            _dirButton.Connect("button_up", this, nameof(OnDirButtonPressed));
+            _dirButton.Text = "Dir: " + _container.SortDirection;
+        }
+        catch (Exception e)
+        {
+            
+        }
     }
 
     public override void _Process(float delta)
@@ -33,6 +52,20 @@ public class TaskmasterDock : Control
             //Log.Wf.Editor("Couldn't update Taskmaster's debug labels", e, true);
         }
         
+    }
+
+    private void OnRegularButtonPressed()
+    {
+        _container.SetRegularSizedChildren(!_container.RegularSizeChildren);
+
+        _regularButton.Text = "Regular: " + _container.RegularSizeChildren;
+    }
+
+    private void OnDirButtonPressed()
+    {
+        _container.SetSortDirection(_container.SortDirection == SortDirection.Right ? SortDirection.Left : SortDirection.Right);
+
+        _dirButton.Text = "Dir: " + _container.SortDirection;
     }
 }
 
